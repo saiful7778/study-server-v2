ac8130c414ac8f034b0cce5669ed03e80fa25085
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-05-23 05:26:19 +0600

    add vercel deploy

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const dbUrl = getEnvVar("DB_CONNECT");
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [getEnvVar("FORNTEND_URL"), "http://localhost:5173"],
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
   })
 );
